c97f3ce593837a132c0f006ddc77e70d11f7f9e1
Rob <rking188@gmail.com>
2016-02-09 22:54:30 -0700

    Updated dependencies to be locally stored in Godep

--- db/db.go
+++ db/db.go
@@ -3,8 +3,8 @@ package db
 import (
 	"errors"
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
+	"github.com/rking788/hotseats-api/Godeps/_workspace/src/github.com/jinzhu/gorm"
+	_ "github.com/rking788/hotseats-api/Godeps/_workspace/src/github.com/lib/pq"
 	"os"
 )
 
